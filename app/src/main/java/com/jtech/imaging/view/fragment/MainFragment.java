e45d03c499c9a93c6c56279c1d42d3a3770419c9
姜函 <wuxubaiyang@gmail.com>
2016-09-06 17:56:07 +0800

    test0

--- app/src/main/java/com/jtech/imaging/view/fragment/MainFragment.java
+++ app/src/main/java/com/jtech/imaging/view/fragment/MainFragment.java
@@ -1,19 +1,30 @@
 package com.jtech.imaging.view.fragment;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.jtech.imaging.R;
 import com.jtech.imaging.contract.MainContract;
+import com.jtech.imaging.realm.OauthRealm;
 import com.jtech.imaging.view.fragment.base.BaseFragment;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.Bind;
+import rx.functions.Action1;
 
 /**
  * 测试用fragment
  * Created by wuxubaiyang on 16/4/16.
  */
 public class MainFragment extends BaseFragment<MainContract.Presenter> implements MainContract.View {
+
+    @Bind(R.id.fab)
+    FloatingActionButton floatingActionButton;
 
     /**
      * 创建视图的方法
@@ -41,6 +52,19 @@ public class MainFragment extends BaseFragment<MainContract.Presenter> implement
      */
     @Override
     public void init(Bundle bundle) {
+        RxView.clicks(floatingActionButton)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .subscribe(new FabClick());
+    }
 
+    /**
+     * fab的点击事件
+     */
+    private class FabClick implements Action1<Void> {
+        @Override
+        public void call(Void aVoid) {
+            OauthRealm.getInstance().removeOauthModel();
+            getActivity().finish();
+        }
     }
 }
